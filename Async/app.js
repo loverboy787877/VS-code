93b76448befad7a75454b0293d32b9a612423024
loverboy787877 <93593646+loverboy787877@users.noreply.github.com>
2023-04-13 12:01:49 +0800

    Try commit

--- Async/app.js
+++ Async/app.js
@@ -1,3 +1,14 @@
+//Sync code example
+
+/*function otherFunct() {
+  console.log("we are in another funct");
+  console.log("do some stuff");
+}
+
+console.log("Start");
+otherFunct();
+console.log("End");*/
+
 //Async code example
 console.log("Start");
 
