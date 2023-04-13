c524087105bfd949d4ef242797e6344c65a9636d
loverboy787877 <93593646+loverboy787877@users.noreply.github.com>
2023-04-13 13:43:54 +0800

    aa

---
+++ Async/appCallback.js
@@ -0,0 +1,116 @@
+//Sync code example
+
+/*function otherFunct() {
+  console.log("we are in another funct");
+  console.log("do some stuff");
+}
+
+console.log("Start");
+otherFunct();
+console.log("End");
+
+//Async code example
+console.log("Start");
+
+setTimeout(() => {
+  console.log("We are in the Timeout");
+}, 2000);
+
+console.log("End");
+
+//Callback function
+
+console.log("Start");
+
+function loginUser(email, password, callback) {
+  setTimeout(() => {
+    console.log("Now we have the data");
+    callback({ userEmail: email });
+  }, 5000);
+}
+function getUserVideo(email, callback) {
+  setTimeout(() => {
+    callback(["video1", "video2", "video3"]);
+  }, 3000);
+}
+function videoDetails(video, callback) {
+  setTimeout(() => {
+    callback("This is the title");
+  }, 3000);
+}
+
+const user = loginUser("villaruzranzi@gmail.com", "aliyah22", (user) => {
+  console.log(user);
+  getUserVideo(user.userEmail, (video) => {
+    console.log(video);
+    videoDetails(video, (title) => {
+      console.log(title);
+    });
+  });
+});
+
+console.log("End");*/
+
+//Refactor the Callback function using promise
+
+console.log("Start");
+
+function loginUser(email, password) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Now we have the data");
+      resolve({ userEmail: email });
+    }, 5000);
+  });
+}
+function getUserVideo(email) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["video1", "video2", "video3"]);
+    }, 3000);
+  });
+}
+function videoDetails(video) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("This is the title");
+    }, 3000);
+  });
+}
+
+// loginUser("villaruzranzi@gmail.com", "aliyah22")
+//   .then((user) => getUserVideo(user.userEmail))
+//   .then((videos) => videoDetails(videos[0]))
+//   .then((detail) => console.log(detail));
+
+//SYNC (ASYNC AWAIT)
+async function displayUser() {
+  try {
+    const loggedUser = await loginUser("Ranzi", 12345);
+    const videos = await getUserVideo(loggedUser.userEmail);
+    const detail = await videoDetails(videos[0]);
+    console.log(detail);
+  } catch (err) {
+    console.log("We could not get the videos");
+  }
+}
+
+displayUser();
+
+//console.log("End");
+
+// const yt = new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log("getting stuff from yt");
+//     resolve({ videos: [1, 2, 3, 4, 5] });
+//   }, 2000);
+// });
+
+// const fb = new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log("getting stuff from fb");
+//     resolve({ user: "Name" });
+//   }, 3000);
+// });
+
+// Promise.all([yt, fb]).then((result) => console.log(result));
---
+++ Async/promise.js
@@ -0,0 +1,15 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("We got the user");
+    //resolve({ user: "Gordon" });
+    reject(new Error("User not logged in"));
+  }, 3000);
+});
+
+promise
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => {
+    console.log(err.msg);
+  });
