4c0ead430a257010fe8a1f4fa8213cdbdd11aa45
loverboy787877 <93593646+loverboy787877@users.noreply.github.com>
2023-04-16 14:38:53 +0800

    review arrow function and promise

--- Async/promise.js
+++ Async/promise.js
@@ -1,15 +1,46 @@
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("We got the user.");
-    //resolve({ user: "Gordon" });
-    reject(new Error("User not logged in"));
-  }, 3000);
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("We got the user.");
+//     //resolve({ user: "Gordon" });
+//     reject(new Error("User not logged in"));
+//   }, 3000);
+// });
+
+// promise
+//   .then((user) => {
+//     console.log(user);
+//   })
+//   .catch((err) => {
+//     console.log(err.msg);
+//   });
+
+// let p = new Promise((resolve, reject) => {
+//   let a = 1 + 2;
+//   if (a == 2) {
+//     resolve("Success");
+//   } else {
+//     reject("failed");
+//   }
+// });
+
+// p.then((message) => {
+//   console.log("This is in the then " + message);
+// }).catch((message) => {
+//   console.log("This is in the catch " + message);
+// });
+
+const recordVideoOne = new Promise((resolve, reject) => {
+  resolve("Video 1 Recorded");
+});
+const recordVideoTwo = new Promise((resolve, reject) => {
+  resolve("Video 2 Recorded");
+});
+const recordVideoThree = new Promise((resolve, reject) => {
+  resolve("Video 3 Recorded");
 });
 
-promise
-  .then((user) => {
-    console.log(user);
-  })
-  .catch((err) => {
-    console.log(err.msg);
-  });
+Promise.race([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
+  (messages) => {
+    console.log(messages);
+  }
+);
--- nodemon/index.js
+++ nodemon/index.js
@@ -204,18 +204,65 @@
 //     console.log(err.msg);
 //   });
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("We got the user.");
-    //resolve({ user: "Gordon" });
-    reject(new Error("User not logged in"));
-  }, 3000);
-});
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("We got the user.");
+//     //resolve({ user: "Gordon" });
+//     reject(new Error("User not logged in"));
+//   }, 3000);
+// });
 
-promise
-  .then((user) => {
-    console.log(user);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// promise
+//   .then((user) => {
+//     console.log(user);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//arrow functions
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// let sum2 = (a, b) => a + b;
+
+// function isPositive(number) {
+//   return number >= 0;
+// }
+
+// let isPositive = (number) => number >= 0;
+
+// function randomNumber() {
+//   return Math.random;
+// }
+
+// let randomNumber = () => Math.random;
+
+// document.addEventListener("click", function () {
+//   console.log("click");
+// });
+
+// document.addEventListener("click", () => console.log("click"));
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  printNameArrow() {
+    setTimeout(() => {
+      console.log("Arrow: " + this.name);
+    }, 100);
+  }
+
+  printNameFunction() {
+    setTimeout(function () {
+      console.log("Function: " + this.name);
+    }, 100);
+  }
+}
+
+let person = new Person("Bob");
+person.printNameArrow();
+person.printNameFunction();
